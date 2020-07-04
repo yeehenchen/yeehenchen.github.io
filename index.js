1cad7e3534c6d90a1b4cdcd9402181cf2d032821
yeehenchen <yeehenchen@gmail.com>
2020-07-04 09:29:16 +0800

    init commit, migrate from profile repo

---
+++ index.js
@@ -0,0 +1,15 @@
+function toggleInfo() {
+  const info = document.querySelector('#info-content');
+  const work = document.querySelector('#work-content');
+  info.style.display = 'block';
+  work.style.display = 'none';
+}
+
+function toggleWork() {
+  const info = document.querySelector('#info-content');
+  const work = document.querySelector('#work-content');
+  info.style.display = 'none';
+  work.style.display = 'block';
+}
+
+
